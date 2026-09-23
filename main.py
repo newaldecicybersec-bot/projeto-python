e6d9a176f707966ae81e2ee179ca66522d69f6c7
Aldeci Barbosa <newaldeci.cybersec@gmail.com>
2026-09-23 20:27:33 -0300

    Add initial Python project with greeting function
    # Projeto Inicial - Python para Automação e Análise Básica
    # Autor: Aldeci Barbosa da Costa
    
    def saudacao_projeto():
        print("========================================")
        print(" Bem-vindo ao meu primeiro projeto no GitHub! ")
        print("========================================")
    
    if __name__ == "__main__":
        saudacao_projeto()

---
+++ main.py
@@ -0,0 +1,10 @@
+# Projeto Inicial - Python para Automação e Análise Básica
+# Autor: Aldeci Barbosa da Costa
+
+def saudacao_projeto():
+    print("========================================")
+    print(" Bem-vindo ao meu primeiro projeto no GitHub! ")
+    print("========================================")
+
+if __name__ == "__main__":
+    saudacao_projeto()
